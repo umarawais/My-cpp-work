ab766a4f6f3bc72e25e888278ee073d7472f01fe
Umar Awais <umaarawais@gmail.com>
2023-08-15 09:01:53 +0400

    Create UserInputNo.cpp

---
+++ UserInputNo.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b=1;
+	cout<<"Enter a Number: ";
+	cin>>a;
+	while (b<=10)
+	{
+	
+		cout<<a<<" x "<<b<<" = "<<a*b<<endl;
+		b++;
+	}
+}
